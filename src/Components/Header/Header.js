101d218435dbb34d95d53f09029bbaa5e0838f45
Jared Medeiros <jared14jared@gmail.com>
2022-02-24 21:14:09 -0500

    header made fully responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,17 +7,22 @@ import uploadButton from '/Users/jaredmedeiros/Desktop/jared-medeiros-brainflix/
 const Header = () => {
     return(   
     <header className = "header">
-        <img src = {logo} alt="BrainFlix logo" className = "header__logo"/>
-        <div className = "header__container1">
-            <div className = "header__search-bar">
-            <img src = {search} alt = "search bar" className = "header__search-bar-icon"/>
-            <p>Search</p>
-            </div>
-            <img src = {profilePhoto} alt = "Profile photo" className = "header__profile-photo--mobile"/>
+        <div className = "header_logo-container">
+            <img src = {logo} alt="BrainFlix logo" className = "header__logo"/>
         </div>
-        <div  className = "header__upload-button">
-        <img src = {uploadButton} alt = "upload button" className = "header__upload-button-icon"/>
-        <p className = "header__upload-button-text">UPLOAD</p>
+        <div className = "header__search-and-upload">
+            <div className = "header__inner-container">
+                <div className = "header__search-bar">
+                    <img src = {search} alt = "search bar" className = "header__search-bar-icon"/>
+                    <p>Search</p>
+                </div>
+                <img src = {profilePhoto} alt = "Profile photo" className = "header__profile-photo--mobile"/>
+            </div>
+            <div  className = "header__upload-button">
+                <img src = {uploadButton} alt = "upload button" className = "header__upload-button-icon"/>
+                <p className = "header__upload-button-text">UPLOAD</p>
+            </div>
+            <img src = {profilePhoto} alt = "Profile photo" className = "header__profile-photo--tablet"/>
         </div>
     </header>
     )
