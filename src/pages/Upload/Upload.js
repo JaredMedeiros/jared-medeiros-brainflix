f75b52f09e61e6db6dae21eb7b341386787d633f
Jared Medeiros <jared14jared@gmail.com>
2022-03-06 15:29:19 -0500

    upload page styled at mobile view. still needs to be made responsive at different screen sizes

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,3 +1,4 @@
+import './Upload.scss'
 import thumbnailPhoto from '../../assets/Images/Upload-video-preview.jpg'
 import uploadButton from '../../assets/Icons/upload.svg'
 import { NavLink } from 'react-router-dom'
@@ -7,25 +8,25 @@ import { NavLink } from 'react-router-dom'
 export default function Upload (currentVideo) {
     return (
         <div className = "upload">
-        <h3 className = "upload__title">Upload</h3>
+        <h1 className = "upload__title">Upload Video</h1>
             <div className = "upload__content">
                 <div className = "upload__video-container">
                     <h3>VIDEO THUMBNAIL</h3>
-                    <img src = {thumbnailPhoto} alt = "video thumbnail photo" />
+                    <img className = "upload__video" src = {thumbnailPhoto} alt = "video thumbnail photo" />
                 </div>
                 <div className = "upload__submission-container">
-                    <form>
-                        <label for = "inputTitle" className = "upload__form-label">TITLE YOUR VIDEO</label>
-                        <input type = "text" id = "inputTitle" placeholder = "Add a title to your video"></input>
-                        <label for = "inputUpload" className = "upload__form-label">ADD A VIDEO DESCRIPTION</label>
-                        <textare id = "inputUpload" placeholder = "Add a description to your video"></textare>
+                    <form className = "upload__form">
+                        <label htmlFor = "inputTitle" className = "upload__form-label">TITLE YOUR VIDEO</label>
+                        <input type = "text" id = "inputTitle" className = "upload__input" placeholder = "Add a title to your video"></input>
+                        <label htmlFor = "inputUpload" className = "upload__form-label">ADD A VIDEO DESCRIPTION</label>
+                        <textarea id = "inputDescription" className = "upload__text-area" placeholder = "Add a description to your video"></textarea>
                     </form>
                 </div>
             </div>
             <div className = "upload__cta-container">
                 <NavLink  className = "upload__button-container" to="/">
-                    <img src = {uploadButton} alt = "upload button" className = "header__upload-button-icon"/>
-                    <p className = "header__upload-button-text">UPLOAD</p>
+                    <img src = {uploadButton}/*  onClick = "uploadAlert()"  */alt = "upload button" className = "upload__button-icon"/>
+                    <p className = "upload__button-text">UPLOAD</p>
                 </NavLink>
             </div>
             <div className = "upload__cancel-container">
