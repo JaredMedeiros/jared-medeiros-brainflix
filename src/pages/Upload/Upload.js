614f78a348a8702461405b64e644d835f5969616
Jared Medeiros <jared14jared@gmail.com>
2022-03-06 21:58:34 -0500

    upload page pade fully responsive between and at breakpoints

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -66,30 +66,30 @@ export default class Upload extends React.Component {
         return (
             <div className = "upload">
             <h1 className = "upload__title">Upload Video</h1>
-                <div className = "upload__content">
-                    <div className = "upload__video-container">
-                        <h3>VIDEO THUMBNAIL</h3>
-                        <img className = "upload__video" src = {thumbnailPhoto} alt = "video thumbnail photo" />
-                    </div>
-                    <form onSubmit={this.handleSubmit} className = "upload__form">
-                        <div class = "upload__field-container">
+                <form onSubmit={this.handleSubmit} className = "upload__form">
+                    <div className = "upload__content">
+                        <div className = "upload__video-container">
+                            <h3>VIDEO THUMBNAIL</h3>
+                            <img className = "upload__video" src = {thumbnailPhoto} alt = "video thumbnail photo" />
+                        </div>
+                        <div className = "upload__field-container">
                             <label htmlFor = "inputTitle" className = "upload__form-label">TITLE YOUR VIDEO</label>
                             <input type = "text" id = "inputTitle" className = "upload__input" name = "inputTitle" placeholder = "Add a title to your video" onChange={this.handleChange} value={this.state.inputTitle}></input>
                             <label htmlFor = "inputUpload" className = "upload__form-label">ADD A VIDEO DESCRIPTION</label>
                             <textarea id = "inputDescription" className = "upload__text-area" name = "inputDescription" placeholder = "Add a description to your video"  onChange={this.handleChange} value={this.state.inputDescription}></textarea>
                         </div>
-                        <div className = "upload__cta-container">
-                            <button className = "upload__button-container">
-                                <img src = {uploadButton} alt = "upload button" className = "upload__button-icon"/>
-                                <p className = "upload__button-text">PUBLISH</p>
-                            </button>
-                            <div className = "upload__cancel-container">
-                                <p className = "upload__cancel-text">CANCEL</p>
-                            </div>
+                    </div>        
+                    <div className = "upload__cta-container">
+                        <button className = "upload__button-container">
+                            <img src = {uploadButton} alt = "upload button" className = "upload__button-icon"/>
+                            <p className = "upload__button-text">PUBLISH</p>
+                        </button>
+                        <div className = "upload__cancel-container">
+                            <p className = "upload__cancel-text">CANCEL</p>
                         </div>
-                    </form>
-                </div>
-             </div>
+                    </div>
+                </form>
+            </div>       
         );
     }    
 } 
